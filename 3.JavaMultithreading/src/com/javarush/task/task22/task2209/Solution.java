4a8da357dff919ce618c3fc145a93a09b375f030
Noriaki5 <reva.andrew@gmail.com>
2021-09-16 20:13:55 +0300

    Updated task #2209

--- 3.JavaMultithreading/src/com/javarush/task/task22/task2209/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2209/Solution.java
@@ -12,44 +12,89 @@ import java.util.StringTokenizer;
 
 public class Solution {
     public static void main(String[] args) {
-        String filename = "";
-        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in))) {
-            filename = bufferedReader.readLine();
-        } catch (IOException ignore) {
+        List<String> list = new ArrayList<>();
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+             BufferedReader fileReader = new BufferedReader(new InputStreamReader(new FileInputStream(reader.readLine())))) {
+            while (fileReader.ready()) {
+                list.add(fileReader.readLine());
+            }
+        } catch (IOException ignored) {
         }
-        String[] splitedLine = null;
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(filename))) {
-            splitedLine = bufferedReader.readLine().split(" ");
-        } catch (IOException ignore) {
-        }
 
-
-        StringBuilder result = getLine(splitedLine);
-
-        System.out.println(result.toString());
-    }
-
-    public static StringBuilder getLine(String... words) {
-        if (words.length == 0) return new StringBuilder();
-
-        List<String> wordsArray = new ArrayList<>(Arrays.asList(words));
-        StringBuilder result = new StringBuilder();
-        List<String> resultArray = new ArrayList<>();
-        result.append(wordsArray.get(0)).append(" ");
-        resultArray.add(wordsArray.get(0));
-
-        for (int i = 0; i < wordsArray.size() - 1; i++) {
-            String word = resultArray.get(i);
-            String letter = word.substring(word.length() - 1);
-            for (int j = 0; j < wordsArray.size(); j++) {
-                if (letter.equalsIgnoreCase(wordsArray.get(j).substring(0,1)) && !word.equals(wordsArray.get(j))) {
-                    result.append(wordsArray.get(j)).append(" ");
-                    resultArray.add(wordsArray.get(j));
-                    break;
-                }
+        List<String> resultList = new ArrayList<>();
+        for (String line : list) {
+            StringTokenizer tokenizer = new StringTokenizer(line);
+            while (tokenizer.hasMoreTokens()) {
+                resultList.add(tokenizer.nextToken());
             }
         }
 
-        return result;
+        StringBuilder result = getLine(getWords(resultList));
+        System.out.println(result.toString());
+    }
+
+    private static String[] getWords(List<String> list) {
+        String[] array = new String[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            array[i] = list.get(i);
+        }
+        return array;
+    }
+
+    private static boolean isTheSameChars(String firstWord, String secondWord) {
+        if (firstWord.endsWith(" ")) {
+            firstWord = firstWord.substring(0, firstWord.length() - 1);
+        }
+        return firstWord.isEmpty() || (secondWord != null &&
+                Character.toUpperCase(firstWord.charAt(firstWord.length() - 1)) == Character.toUpperCase(secondWord.charAt(0)));
+    }
+
+    private static <T> T getLastElement(List<? extends T> list) {
+        return list.get(list.size() - 1);
+    }
+
+
+    public static StringBuilder getLine(String... words) {
+        StringBuilder builder = new StringBuilder();
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < words.length; i++) {
+            list.add(i);
+            if (findSolutions(list, words)) {
+                for (Integer integer : list) {
+                    builder.append(words[integer]);
+                    builder.append(" ");
+                }
+                return builder;
+            }
+            list.remove(Integer.valueOf(i));
+        }
+
+        return builder;
+    }
+
+    private static boolean findSolutions(List<Integer> list, String... words) {
+        if (list.size() == words.length) {
+            return true;
+        }
+        for (int i = 0; i < words.length; i++) {
+            if (isValid(list, words[i], words)) {
+                list.add(i);
+                if (findSolutions(list, words)) {
+                    return true;
+                }
+                list.remove(Integer.valueOf(i));
+            }
+        }
+        return false;
+    }
+
+    private static boolean isValid(List<Integer> list, String word, String... words) {
+        for (Integer integer : list) {
+            if (words[integer].equals(word)) {
+                return false;
+            }
+        }
+        return isTheSameChars(words[getLastElement(list)], word);
     }
 }
