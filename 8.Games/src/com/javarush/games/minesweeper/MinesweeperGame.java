f77f179bb31a63438edae3dffb9e6f4f84babb97
Noriaki5 <reva.andrew@gmail.com>
2021-09-19 23:22:43 +0300

    Updated game Minesweeper Added method createGame

--- 8.Games/src/com/javarush/games/minesweeper/MinesweeperGame.java
+++ 8.Games/src/com/javarush/games/minesweeper/MinesweeperGame.java
@@ -4,9 +4,20 @@ import com.javarush.engine.cell.*;
 
 public class MinesweeperGame extends Game {
     final private static int SIDE = 9;
+    private GameObject[][] gameField = new GameObject[SIDE][SIDE];
 
     @Override
     public void initialize() {
         setScreenSize(SIDE, SIDE);
+        createGame();
+    }
+
+    private void createGame() {
+        for (int i = 0; i < gameField.length; i++) {
+            for (int j = 0; j < gameField[i].length; j++) {
+                gameField[j][i] = new GameObject(i, j);
+                setCellColor(i, j, Color.CORNFLOWERBLUE);
+            }
+        }
     }
 }
