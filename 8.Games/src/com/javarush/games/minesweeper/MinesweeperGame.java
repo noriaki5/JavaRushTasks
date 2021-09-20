a393689047a7bddd3d16a15e2029fab7a7a3f0c9
Noriaki5 <reva.andrew@gmail.com>
2021-09-20 22:34:18 +0300

    Updated game Minesweeper Added neighbors mines count method

--- 8.Games/src/com/javarush/games/minesweeper/MinesweeperGame.java
+++ 8.Games/src/com/javarush/games/minesweeper/MinesweeperGame.java
@@ -1,9 +1,14 @@
 package com.javarush.games.minesweeper;
 
-import com.javarush.engine.cell.*;
+import com.javarush.engine.cell.Color;
+import com.javarush.engine.cell.Game;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MinesweeperGame extends Game {
-    final private static int SIDE = 9;
+    private static final int SIDE = 9;
     private GameObject[][] gameField = new GameObject[SIDE][SIDE];
     private int countMinesOnField;
 
@@ -14,13 +19,58 @@ public class MinesweeperGame extends Game {
     }
 
     private void createGame() {
-        for (int i = 0; i < gameField.length; i++) {
-            for (int j = 0; j < gameField[i].length; j++) {
-                boolean isMine = getRandomNumber(10) == 9;
-                if (isMine) countMinesOnField++;
-                gameField[j][i] = new GameObject(i, j, isMine);
-                setCellColor(i, j, Color.CORNFLOWERBLUE);
+        for (int y = 0; y < SIDE; y++) {
+            for (int x = 0; x < SIDE; x++) {
+                boolean isMine = getRandomNumber(10) < 1;
+                if (isMine) {
+                    countMinesOnField++;
+                }
+                gameField[y][x] = new GameObject(x, y, isMine);
+                setCellColor(x, y, Color.ORANGE);
             }
         }
+        countMineNeighbors();
+    }
+
+    private List<GameObject> getNeighbors(GameObject gameObject) {
+        List<GameObject> result = new ArrayList<>();
+        for (int y = gameObject.y - 1; y <= gameObject.y + 1; y++) {
+            for (int x = gameObject.x - 1; x <= gameObject.x + 1; x++) {
+                if (y < 0 || y >= SIDE) {
+                    continue;
+                }
+                if (x < 0 || x >= SIDE) {
+                    continue;
+                }
+                if (gameField[y][x] == gameObject) {
+                    continue;
+                }
+                result.add(gameField[y][x]);
+            }
+        }
+        return result;
+    }
+
+    private void countMineNeighbors() {
+        for (int y = 0; y < SIDE; y++) {
+            for (int x = 0; x < SIDE; x++) {
+                GameObject gameObject = gameField[y][x];
+                if (!gameObject.isMine) {
+                    for (GameObject neighbor : getNeighbors(gameObject)) {
+                        if (neighbor.isMine) {
+                            gameObject.countMineNeighbors++;
+                        }
+                    }
+                }
+            }
+        }
+//        Validator don't accept lambda :(
+//        for (int y = 0; y < gameField.length; y++) {
+//            for (int x = 0; x < gameField[y].length; x++) {
+//                if (!gameField[y][x].isMine) {
+//                    getNeighbors(gameField[y][x]).forEach(gameObject -> {if (gameObject.isMine) gameObject.countMineNeighbors++;});
+//                }
+//            }
+//        }
     }
 }
