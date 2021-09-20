2848a85776e545c209dfe1161a00a79d4b58110d
Noriaki5 <reva.andrew@gmail.com>
2021-09-20 22:08:24 +0300

    Updated game Minesweeper Added mines generation

--- 8.Games/src/com/javarush/games/minesweeper/MinesweeperGame.java
+++ 8.Games/src/com/javarush/games/minesweeper/MinesweeperGame.java
@@ -5,6 +5,7 @@ import com.javarush.engine.cell.*;
 public class MinesweeperGame extends Game {
     final private static int SIDE = 9;
     private GameObject[][] gameField = new GameObject[SIDE][SIDE];
+    private int countMinesOnField;
 
     @Override
     public void initialize() {
@@ -15,7 +16,9 @@ public class MinesweeperGame extends Game {
     private void createGame() {
         for (int i = 0; i < gameField.length; i++) {
             for (int j = 0; j < gameField[i].length; j++) {
-                gameField[j][i] = new GameObject(i, j);
+                boolean isMine = getRandomNumber(10) == 9;
+                if (isMine) countMinesOnField++;
+                gameField[j][i] = new GameObject(i, j, isMine);
                 setCellColor(i, j, Color.CORNFLOWERBLUE);
             }
         }
