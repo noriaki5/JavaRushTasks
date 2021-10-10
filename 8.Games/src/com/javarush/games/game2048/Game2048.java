c2f3be12bf9f604504959c0a22991fc6d70375e5
Noriaki5 <reva.andrew@gmail.com>
2021-10-10 11:23:53 +0300

    Updated game 2048 Added gameFiled drawing

--- 8.Games/src/com/javarush/games/game2048/Game2048.java
+++ 8.Games/src/com/javarush/games/game2048/Game2048.java
@@ -4,9 +4,24 @@ import com.javarush.engine.cell.*;
 
 public class Game2048 extends Game {
     private static final int SIDE = 4;
+    private int[][] gameField = new int[SIDE][SIDE];
 
     @Override
     public void initialize() {
         setScreenSize(SIDE, SIDE);
+        createGame();
+        drawScene();
+    }
+
+    private void createGame() {
+
+    }
+
+    private void drawScene() {
+        for (int i = 0; i < SIDE; i++) {
+            for (int j = 0; j < SIDE; j++) {
+                setCellColor(i, j, Color.AQUA);
+            }
+        }
     }
 }
