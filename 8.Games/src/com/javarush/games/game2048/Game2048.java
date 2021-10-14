8a30328daae34b57aa532b7f7b431e543dad4f13
Noriaki5 <reva.andrew@gmail.com>
2021-10-14 14:51:24 +0300

    Updated game 2048 Implemented all moves

--- 8.Games/src/com/javarush/games/game2048/Game2048.java
+++ 8.Games/src/com/javarush/games/game2048/Game2048.java
@@ -136,15 +136,27 @@ public class Game2048 extends Game {
     }
 
     private void moveUp() {
-
+        rotateClockwise();
+        rotateClockwise();
+        rotateClockwise();
+        moveLeft();
+        rotateClockwise();
     }
 
     private void moveRight() {
-
+        rotateClockwise();
+        rotateClockwise();
+        moveLeft();
+        rotateClockwise();
+        rotateClockwise();
     }
 
     private void moveDown() {
-
+        rotateClockwise();
+        moveLeft();
+        rotateClockwise();
+        rotateClockwise();
+        rotateClockwise();
     }
 
     private void rotateClockwise() {
